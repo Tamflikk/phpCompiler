3572397501b0409f3d8343fbb6df7285010586b0
Tamflikk <alejandrotamayo32200@gmail.com>
2024-03-06 08:16:12 -0400

    update lexer.js

--- lexer.js
+++ lexer.js
@@ -1,38 +1,42 @@
 class Lexer {
-    constructor(input) {
-        this.input = input;
-        this.tokens = [];
-        this.tokenize();
-    }
+  constructor(input) {
+    this.input = input;
+    this.tokens = [];
+    this.tokenize();
+  }
 
-    tokenize() {
-        const tokenSpecs = [
-            ['NUMBER', /^\d+/], 
-            ['STRING', /^"[^"]*"/], 
-            ['WHITESPACE', /^\s+/, true], // Ignorado
-            ['KEYWORD', /^(if|else|return|function)\b/], 
-            ['IDENTIFIER', /^[a-zA-Z_]\w*/],
-            ['OPERATOR', /^[+*/=-]/],
-        ];
+  tokenize() {
+    const tokenSpecs = [
+      ["NUMBER", /^\d+/],
+      ["STRING", /^"[^"]*"/],
+      ["WHITESPACE", /^\s+/, true], // Ignorado
+      ["KEYWORD", /^(if|else|return|function)\b/],
+      ["IDENTIFIER", /^[a-zA-Z_]\w*/],
+      ["OPERATOR", /^[+*/=-]/],
 
-        let m;
-        while (this.input.length > 0) {
-            let matchFound = false;
+      ["SEMICOLON", /^;/], // Para el punto y coma
+      ["LPAREN", /^\(/], // Para el paréntesis abierto
+      ["RPAREN", /^\)/], // Para el paréntesis cerrado
+    ];
 
-            for (let [type, regex, ignore] of tokenSpecs) {
-                if (m = this.input.match(regex)) {
-                    if (!ignore) {
-                        this.tokens.push({type, value: m[0]});
-                    }
-                    this.input = this.input.slice(m[0].length); // Actualiza la entrada
-                    matchFound = true;
-                    break;
-                }
-            }
+    let m;
+    while (this.input.length > 0) {
+      let matchFound = false;
 
-            if (!matchFound) throw new Error(`Unknown token: ${this.input}`);
+      for (let [type, regex, ignore] of tokenSpecs) {
+        if ((m = this.input.match(regex))) {
+          if (!ignore) {
+            this.tokens.push({ type, value: m[0] });
+          }
+          this.input = this.input.slice(m[0].length); // Actualiza la entrada
+          matchFound = true;
+          break;
         }
+      }
+
+      if (!matchFound) throw new Error(`Unknown token: ${this.input}`);
     }
+  }
 }
 
 module.exports = { Lexer };

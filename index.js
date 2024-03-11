b71f5ce3d41793a0843b7d7e9584d715c1f5e6da
tamflikk <alejandrotamayo32200@gmail.com>
2024-03-10 23:26:53 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
-const Lexer = require('./lexer'); // Asume que lexer.js está en el mismo directorio
-const Parser = require('./parser'); // Asume que parser.js está en el mismo directorio
+const Lexer = require('./lexer');
+const Parser = require('./parser');
+const IntermediateCodeGenerator = require('./intermediate_code_generator');
 
 const input = fs.readFileSync('test.php', 'utf8');
 const lexer = new Lexer(input);
 const parser = new Parser(lexer.tokens);
 const ast = parser.parse();
 
-console.log(JSON.stringify(ast, null, 2));
+const generator = new IntermediateCodeGenerator(ast);
+const intermediateCode = generator.generate();
+
+console.log(intermediateCode.join('\n'));
---
+++ intermediate_code_generator.js
@@ -0,0 +1,51 @@
+class IntermediateCodeGenerator {
+    constructor(ast) {
+      this.ast = ast;
+      this.code = [];
+      this.tempCount = 0;
+    }
+  
+    generate() {
+      this.generateCode(this.ast);
+      return this.code;
+    }
+  
+    generateCode(node) {
+        switch (node.type) {
+          case 'Program':
+            return node.body.flatMap(this.generateCode.bind(this));
+          case 'AssignmentExpression':
+            return this.generateAssignment(node);
+          case 'BinaryExpression':
+            return this.generateBinaryExpression(node);
+          case 'NumericLiteral':
+            return node.value;
+          case 'VariableReference':
+            return node.name;
+          // Agrega más casos según los tipos de nodos que tengas en tu AST
+          default:
+            throw new Error(`Tipo de nodo no soportado: ${node.type}`);
+        }
+      }
+
+      generateAssignment(node) {
+        const tempVar = this.newTemp();
+        const value = this.generateCode(node.value);
+        this.code.push(`${tempVar} = ${value}`);
+        this.code.push(`${node.name} = ${tempVar}`);
+      }
+      
+      generateBinaryExpression(node) {
+        const tempVar = this.newTemp();
+        const left = this.generateCode(node.left);
+        const right = this.generateCode(node.right);
+        this.code.push(`${tempVar} = ${left} ${node.operator} ${right}`);
+        return tempVar;
+      }
+  
+    newTemp() {
+      return `t${this.tempCount++}`;
+    }
+  }
+  
+  module.exports = IntermediateCodeGenerator;

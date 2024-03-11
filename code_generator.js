99f60d5202d15c07ae01734d12696ab1bde20ff7
tamflikk <alejandrotamayo32200@gmail.com>
2024-03-11 08:06:13 -0400

    update index.js and test.php

---
+++ code_generator.js
@@ -0,0 +1,23 @@
+class CodeGenerator {
+    constructor(intermediateCode) {
+      this.intermediateCode = intermediateCode;
+      this.objectCode = [];
+    }
+  
+    generate() {
+        for (const instruction of this.intermediateCode) {
+          const [target, operation, operand1, operand2] = instruction.split(' ');
+          if (operation === '=') {
+            this.objectCode.push(`mov ${target}, ${operand1}`);
+          } else if (operation === '+') {
+            this.objectCode.push(`mov eax, ${operand1}`);
+            this.objectCode.push(`add eax, ${operand2}`);
+            this.objectCode.push(`mov ${target}, eax`);
+          }
+          // Agregar mas casos segun las operaciones que soporte el codigo intermedio
+        }
+        return this.objectCode;
+      }
+  }
+  
+  module.exports = CodeGenerator;
--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const fs = require('fs');
 const Lexer = require('./lexer');
 const Parser = require('./parser');
 const IntermediateCodeGenerator = require('./intermediate_code_generator');
+const CodeGenerator = require('./code_generator');
+
+const input = fs.readFileSync('test.php', 'utf8');
+const lexer = new Lexer(input);
+const parser = new Parser(lexer.tokens);
+const ast = parser.parse();
+
+const intermediateCodeGenerator = new IntermediateCodeGenerator(ast);
+const intermediateCode = intermediateCodeGenerator.generate();
+
+const codeGenerator = new CodeGenerator(intermediateCode);
+const objectCode = codeGenerator.generate();
+
+console.log(objectCode.join('\n'));
+
+
+/*
+const fs = require('fs');
+const Lexer = require('./lexer');
+const Parser = require('./parser');
+const IntermediateCodeGenerator = require('./intermediate_code_generator');
 
 const input = fs.readFileSync('test.php', 'utf8');
 const lexer = new Lexer(input);
@@ -12,3 +33,4 @@ const generator = new IntermediateCodeGenerator(ast);
 const intermediateCode = generator.generate();
 
 console.log(intermediateCode.join('\n'));
+*/
